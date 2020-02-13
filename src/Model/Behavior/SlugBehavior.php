3006acda29ee78bb5f27a145579b90e76d9f915b
kicaj <kicaj@kdev.pl>
2020-02-13 22:13:00 +0100

    Rebuild Slug behavior

--- src/Model/Behavior/SlugBehavior.php
+++ src/Model/Behavior/SlugBehavior.php
@@ -1,9 +1,10 @@
 <?php
 namespace Slug\Model\Behavior;
 
-use Cake\ORM\Behavior;
-use Cake\Event\Event;
 use Cake\Datasource\EntityInterface;
+use Cake\Event\Event;
+use Cake\ORM\Behavior;
+use Cake\ORM\Query;
 use Cake\Utility\Text;
 use Slug\Exception\FieldException;
 use Slug\Exception\FieldTypeException;
@@ -16,194 +17,195 @@ class SlugBehavior extends Behavior
 {
 
     /**
-     * Default replacement string
+     * Default config.
      *
-     * @var string
+     * @var array
      */
-    protected $_defaultReplacement = '-';
+    public $defaultConfig = [
+        'source' => 'name',
+        'replacement' => '-',
+        'finder' => 'list',
+        'length' => 255,
+    ];
 
     /**
-     * Default field to create slug
-     *
-     * @var string
+     * {@inheritdoc}
      */
-    protected $_defaultField = 'title';
+    public function initialize(array $config): void
+    {
+        parent::initialize($config);
+
+        if (empty($this->getConfig())) {
+            $this->setConfig('slug', $this->defaultConfig);
+        }
+
+        foreach ($this->getConfig() as $target => $config) {
+            if (!is_array($config)) {
+                $this->_configDelete($target);
+
+                $target = $config;
+
+                $this->setConfig($target, $this->defaultConfig);
+            } else {
+                $this->setConfig($target, array_merge($this->defaultConfig, $config));
+            }
+
+            if (!$this->getTable()->hasField($target)) {
+                throw new FieldException(__d('slug', 'Cannot find target {0} field in schema.', $target));
+            } elseif (!$this->getTable()->hasField($this->getConfig($target . '.source'))) {
+                throw new FieldException(__d('slug', 'Cannot find source {0} field in schema.', $this->getConfig($target . '.source')));
+            }
+
+            if ($this->getTable()->getSchema()->getColumnType($target) !== 'string') {
+                throw new FieldTypeException(__d('slug', 'Target field {0} should be string type.', $target));
+            }
+        }
+    }
 
     /**
-     * Default finder method
-     *
-     * @var string
+     * {@inheritdoc}
      */
-    protected $_defaultFinder = 'list';
+    public function beforeFind(Event $event, Query $query, $options)
+    {
+        $query->select(array_keys($this->getConfig()));
+    }
 
     /**
      * {@inheritdoc}
      */
     public function beforeSave(Event $event, EntityInterface $entity)
     {
-        if (empty($this->_config)) {
-            $this->_config['slug'] = [];
-        }
-
-        foreach ($this->_config as $slug => $config) {
-            if (!is_array($config)) {
-                $slug = $config;
+        foreach ($this->getConfig() as $target => $config) {
+            if (!isset($config['present']) || $config['present'] !== true) {
+                $entity->{$target} = $this->createSlug($entity, $target);
             }
+        }
+    }
 
-            if (!isset($this->_config[$slug]['present']) || $this->_config[$slug]['present'] !== true) {
-                if (!isset($this->_config[$slug]['field'])) {
-                    $this->_config[$slug]['field'] = $this->_defaultField;
+    /**
+     * Create slug.
+     *
+     * @param EntityInterface $entity Entity.
+     * @param string $target Target slug field name.
+     * @return string Slug.
+     */
+    public function createSlug(EntityInterface $entity, string $target): string
+    {
+        $config = $this->getConfig($target);
+
+        if ($entity->isDirty($config['source'])) {
+            if ((mb_strlen($config['replacement']) + 1) < $config['length']) {
+                if (isset($config['method'])) {
+                    if (method_exists($this->getTable(), $config['method'])) {
+                        $slug = $this->getTable()->{$config['method']}($entity->{$config['source']}, $config);
+                    } else {
+                        throw new MethodException(__d('slug', 'Method {0} does not exist.', $config['method']));
+                    }
+                } else {
+                    $slug = Text::slug(mb_strtolower($entity->{$config['source']}), [
+                        'replacement' => $config['replacement'],
+                    ]);
                 }
 
-                if ($this->_table->hasField($slug)) {
-                    if ($this->_table->hasField($this->_config[$slug]['field'])) {
-                        $schema = $this->_table->getSchema()->getColumn($slug);
+                $slugs = $this->sortSlugs($this->getSlugs($slug, $target));
 
-                        if ($schema['type'] == 'string') {
-                            if (!isset($this->_config[$slug]['replacement'])) {
-                                $this->_config[$slug]['replacement'] = $this->_defaultReplacement;
-                            }
+                // Slug is just numbers
+                if (preg_match('/^[0-9]+$/', $slug)) {
+                    $numbers = preg_grep('/^[0-9]+$/', $slugs);
 
-                            if (!isset($this->_config[$slug]['length']) || $this->_config[$slug]['length'] > $schema['length']) {
-                                $this->_config[$slug]['length'] = $schema['length'];
-                            }
+                    if (!empty($numbers)) {
+                        sort($numbers);
 
-                            if (!isset($this->_config[$slug]['finder'])) {
-                                $this->_config[$slug]['finder'] = $this->_defaultFinder;
-                            }
+                        $slug = end($numbers);
 
-                            $entity->{$slug} = $this->createSlug($entity->{$this->_config[$slug]['field']}, $slug);
+                        $slug++;
+                    }
+                }
+
+                // Cut slug
+                if (mb_strlen($replace = preg_replace('/\s+/', $config['replacement'], $slug)) > $config['length']) {
+                    $slug = mb_substr($replace, 0, $config['length']);
+
+                    // Update slug list based on cut slug
+                    $slugs = $this->sortSlugs($this->getSlugs($slug, $field));
+                }
+
+                $slug = preg_replace('/' . preg_quote($config['replacement']) . '$/', '', trim(mb_substr($slug, 0, $config['length'])));
+
+                if (in_array($slug, $slugs)) {
+                    $list = preg_grep('/^' . preg_replace('/' . preg_quote($config['replacement']) . '([1-9]{1}[0-9]*)$/', $config['replacement'], $slug) . '/', $slugs);
+
+                    preg_match('/^(.*)' . preg_quote($config['replacement']) . '([1-9]{1}[0-9]*)$/', end($list), $matches);
+
+                    if (empty($matches)) {
+                        $increment = 1;
+                    } else {
+                        if (isset($matches[2])) {
+                            $increment = $matches[2] += 1;
                         } else {
-                            throw new FieldTypeException(__d('slug', 'Field {s} should be string type.', $slug));
+                            throw new IncrementException(__d('slug', 'Cannot create next suffix because matches are empty.'));
+                        }
+                    }
+
+                    if (mb_strlen($slug . $config['replacement'] . $increment) <= $config['length']) {
+                        $string = $slug;
+                    } elseif (mb_strlen(mb_substr($slug, 0, -mb_strlen($increment))) + mb_strlen($config['replacement'] . $increment) <= $config['length']) {
+                        $string = mb_substr($slug, 0, $config['length'] - mb_strlen($config['replacement'] . $increment));
+                    } else {
+                        $string = mb_substr($slug, 0, -(mb_strlen($config['replacement'] . $increment)));
+                    }
+
+                    if (mb_strlen($string) > 0) {
+                        $slug = $string . $config['replacement'] . $increment;
+
+                        // Refresh slugs list
+                        $slugs = $this->sortSlugs(array_merge($slugs, $this->getSlugs($slug, $target)));
+
+                        if (in_array($slug, $slugs)) {
+                            return $this->createSlug($slug, $target);
                         }
                     } else {
-                        throw new FieldException(__d('slug', 'Cannot find {0} field as source in schema.', $this->_config[$slug]['field']));
+                        throw new LengthException(__d('slug', 'Cannot create slug because there are no available names.'));
                     }
-                } else {
-                    throw new FieldException(__d('slug', 'Cannot find {0} field in schema.', $slug));
                 }
-            }
-        }
-    }
 
-    /**
-     * Create unique slug
-     *
-     * @param string $slug String to slug
-     * @param string $field Slug field name
-     * @return string Slug
-     */
-    public function createSlug($slug, $field)
-    {
-        if ((mb_strlen($this->_config[$field]['replacement']) + 1) < $this->_config[$field]['length']) {
-            if (isset($this->_config[$field]['method'])) {
-                if (method_exists($this->_table, $this->_config[$field]['method'])) {
-                    $slug = $this->_table->{$this->_config[$field]['method']}($slug, $this->_config[$field]['replacement']);
-                } else {
-                    throw new MethodException(__d('slug', 'Method {0} does not exist.', $this->_config[$field]['method']));
-                }
+                return $slug;
             } else {
-                $slug = Text::slug(mb_strtolower($slug), [
-                    'replacement' => $this->_config[$field]['replacement']
-                ]);
+                throw new LimitException(__d('slug', 'Limit of length in {0} field is too short.', $target));
             }
-
-            $slugs = $this->_sortSlugs($this->_getSlugs($slug, $field));
-
-            // Slug is just numbers
-            if (preg_match('/^[0-9]+$/', $slug)) {
-                $numbers = preg_grep('/^[0-9]+$/', $slugs);
-
-                if (!empty($numbers)) {
-                    sort($numbers);
-
-                    $slug = end($numbers);
-
-                    $slug++;
-                }
-            }
-
-            // Cut slug
-            if (mb_strlen($replace = preg_replace('/\s+/', $this->_config[$field]['replacement'], $slug)) > $this->_config[$field]['length']) {
-                $slug = mb_substr($replace, 0, $this->_config[$field]['length']);
-
-                // Update slug list based on cut slug
-                $slugs = $this->_sortSlugs($this->_getSlugs($slug, $field));
-            }
-
-            $slug = preg_replace('/' . preg_quote($this->_config[$field]['replacement']) . '$/', '', trim(mb_substr($slug, 0, $this->_config[$field]['length'])));
-
-            if (in_array($slug, $slugs)) {
-                $list = preg_grep('/^' . preg_replace('/' . preg_quote($this->_config[$field]['replacement']) . '([1-9]{1}[0-9]*)$/', $this->_config[$field]['replacement'], $slug) . '/', $slugs);
-
-                preg_match('/^(.*)' . preg_quote($this->_config[$field]['replacement']) . '([1-9]{1}[0-9]*)$/', end($list), $matches);
-
-                if (empty($matches)) {
-                    $increment = 1;
-                } else {
-                    if (isset($matches[2])) {
-                        $increment = $matches[2] += 1;
-                    } else {
-                        throw new IncrementException(__d('slug', 'Cannot create next suffix because matches are empty.'));
-                    }
-                }
-
-                if (mb_strlen($slug . $this->_config[$field]['replacement'] . $increment) <= $this->_config[$field]['length']) {
-                    $string = $slug;
-                } elseif (mb_strlen(mb_substr($slug, 0, -mb_strlen($increment))) + mb_strlen($this->_config[$field]['replacement'] . $increment) <= $this->_config[$field]['length']) {
-                    $string = mb_substr($slug, 0, $this->_config[$field]['length'] - mb_strlen($this->_config[$field]['replacement'] . $increment));
-                } else {
-                    $string = mb_substr($slug, 0, -(mb_strlen($this->_config[$field]['replacement'] . $increment)));
-                }
-
-                if (mb_strlen($string) > 0) {
-                    $slug = $string . $this->_config[$field]['replacement'] . $increment;
-
-                    // Refresh slugs list
-                    $slugs = $this->_sortSlugs(array_merge($slugs, $this->_getSlugs($slug, $field)));
-
-                    if (in_array($slug, $slugs)) {
-                        return $this->createSlug($slug, $field);
-                    }
-                } else {
-                    throw new LengthException(__d('slug', 'Cannot create slug because there are no available names.'));
-                }
-            }
-
-            return $slug;
         } else {
-            throw new LimitException(__d('slug', 'Limit of length in {0} field is too short.', $field));
+            return $entity->{$target};
         }
     }
 
     /**
-     * Get exists slug list
+     * Get existing slug list.
      *
-     * @param string $slug String to slug
-     * @param string $field Slug field name
-     * @return array List of slugs
+     * @param string $slug Slug to find.
+     * @param string $target Target slug field name.
+     * @return array List of slugs.
      */
-    protected function _getSlugs($slug, $field)
+    protected function getSlugs(string $slug, string $target): array
     {
-        return $this->_table->find($this->_config[$field]['finder'], [
-            'valueField' => $field,
+        return $this->getTable()->find($this->getConfig($target . '.finder'), [
+            'valueField' => $target,
         ])->where([
             'OR' => [
-                $this->_table->getAlias() . '.' . $field => $slug,
-                $this->_table->getAlias() . '.' . $field . ' REGEXP' => '^' . preg_replace('/' . preg_quote($this->_config[$field]['replacement']) . '([1-9]{1}[0-9]*)$/', '', $slug),
+                $this->getTable()->getAlias() . '.' . $target => $slug,
+                $this->getTable()->getAlias() . '.' . $target . ' REGEXP' => '^' . preg_replace('/' . preg_quote($this->getConfig($target . '.replacement')) . '([1-9]{1}[0-9]*)$/', '', $slug),
             ],
         ])->order([
-            $this->_table->getAlias() . '.' . $field => 'ASC',
+            $this->getTable()->getAlias() . '.' . $target => 'ASC',
         ])->toArray();
     }
 
     /**
-     * Sort slug list in normal mode
+     * Sort slug list in normal mode.
      *
-     * @param array $slugs Slug list
-     * @return array Slug list in normal mode
+     * @param array $slugs Slug list.
+     * @return array Slug list.
      */
-    protected function _sortSlugs($slugs)
+    protected function sortSlugs(array $slugs): array
     {
         if (!empty($slugs)) {
             $slugs = array_unique($slugs);
